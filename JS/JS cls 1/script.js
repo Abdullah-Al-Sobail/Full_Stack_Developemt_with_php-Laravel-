bdb8ec14db4a1e984c8b91101c6211415a9100c5
muaz64 <muazmuhammad21@gmail.com>
2025-05-04 23:17:00 +0600

    js d1

---
+++ JS/JS cls 1/script.js
@@ -0,0 +1,15 @@
+/*console.log(5+6)
+document.getElementById("demo").innerHTML ="i'm javascript";
+document.write(5+6);*/
+let a,b,c ;
+a=5
+b=10
+c=a*b
+document.write(c);
+// console.log("muaz")
+
+// single line
+
+/*Multi-line
+................
+.............. */
