a12b8802e2d2ec4d55f46e0b16ca288bce1f18a7
muaz64 <muazmuhammad21@gmail.com>
2025-05-06 22:37:12 +0600

    js cls 2

---
+++ JavaScript/Class02/script.js
@@ -0,0 +1,37 @@
+// var a= 'Tashfiya';
+// a="aiman";
+// var a= "saima";
+// var _a = 'mahzabin';
+// let myName ="Muaz";
+// let myName1 ="Rahat";
+// let x =5
+// {
+//     let x=2;
+// }
+// myId
+// fromToHandle
+// const Pi =3.1416;
+// pi=3.14
+// pi =pi+100;
+// const PI=3.1416;
+// {
+// const PI =3.14161;
+// }
+// {
+//     const PI =3.14161519;
+//     }
+// console.log(PI);
+ var num =10;
+// var num1 =10.10;
+var text = "i'm javascript";
+var text1= 'i love js'
+// var isString ='100';
+// var isTrue= true;
+// var isTrue = false
+// var nothing =null;
+var me = text + text1;
+var M= 'Hello'+ "i'm not java!"
+var myName ='Muaz';
+myName='Muhammad';
+console.log(M)
+
