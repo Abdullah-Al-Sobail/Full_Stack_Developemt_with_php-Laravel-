2d270eb3d109d5340898433b9abaa704af55e753
muaz64 <muazmuhammad21@gmail.com>
2025-05-10 22:57:05 +0600

    js cls4 up1

---
+++ JavaScript/Class04/script.js
@@ -0,0 +1,39 @@
+
+// function funcName(){
+//     console.log('hello i am from function');
+// }
+
+// funcName();
+
+//   var funcName = function(){
+//     console.log('hello i am from function')
+//   }
+//   funcName();
+
+//   function sum_mechine(num1, num2){
+//     sum = num1 + num2 ;
+//      return sum;
+//  }
+// console.log(sum_mechine(2 ,2));
+// console.log(sum_mechine(4 ,"m"));
+
+// function callMyName(name, call){
+//     var myAge ="25";
+//     call(myAge);
+//     console.log('Hello i am Mr.' + name);
+// }
+// function hello (xyz) {
+//     console.log( 'My age is:' + xyz);
+// }
+
+// callMyName('Muaz', hello);
+// console.log(this)
+// function hellothis(){
+//     'use strict';
+//     console.log(this);
+// }
+// hellothis();
+function unName(){
+    this.name = 'Muaz';
+}
+unName();
