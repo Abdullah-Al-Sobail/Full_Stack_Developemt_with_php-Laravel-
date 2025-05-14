fb9a10dedf231e29ce97e5a9d48a0c23cad53c4f
muaz64 <muazmuhammad21@gmail.com>
2025-05-14 22:04:49 +0600

    js cls06 upt1

---
+++ JavaScript/Class06/script.js
@@ -0,0 +1,16 @@
+// for (var i=0; i<10 ; i++) {
+//     console.log('Go ' + i + ' step');
+// }
+
+// var num = 0
+// while (num<10){
+//     num = prompt( 'Enter the number: ');
+// }
+//  console.log ('out of the loop');
+
+ var number;
+ do {
+    console.log('inside the loop');
+    number = prompt('Enter the value: ');
+ } while (number<10);
+ console.log('outside of loop');
