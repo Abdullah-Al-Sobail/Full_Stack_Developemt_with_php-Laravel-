bc28bfa32a3726d5b656cd9624ef0f41bc348748
muaz64 <muazmuhammad21@gmail.com>
2025-05-14 23:52:24 +0600

    js cls06 upt2

--- JavaScript/Class06/script.js
+++ JavaScript/Class06/script.js
@@ -2,15 +2,65 @@
 //     console.log('Go ' + i + ' step');
 // }
 
-// var num = 0
-// while (num<10){
-//     num = prompt( 'Enter the number: ');
-// }
-//  console.log ('out of the loop');
+// console.log('inside the loop');console.log('inside the loop');
 
- var number;
- do {
-    console.log('inside the loop');
-    number = prompt('Enter the value: ');
- } while (number<10);
- console.log('outside of loop');
+//  var number;
+//   do {
+//      console.log('inside the loop');
+//     number = prompt('Enter the value: ');
+//  } while (number<10);
+//  console.log('outside of loop');
+
+// const student = {name: "Muaz", age : 25, country : 'Bd'};
+//  for (let key in student){
+//     console.log( key + ":" + student[key]);
+//  }
+
+//  let fruits = ["Mango", "Jack", "Banana", "Apple"];
+//  for (let fruit of fruits) {
+//     console.log(fruit);
+//  }
+
+function runForLoop() {
+  let text = "";
+  for (let i = 1; i <= 5; i++) {
+    text += "For Loop: " + i + "<br>";
+  }
+  document.getElementById("output").innerHTML = text;
+}
+
+function runWhileLoop() {
+  let i = 1, text = "";
+  while (i <= 5) {
+    text += "While Loop: " + i + "<br>";
+    i++;
+  }
+  document.getElementById("output").innerHTML = text;
+}
+
+function runDoWhileLoop() {
+  let i = 1, text = "";
+  do {
+    text += "Do...While Loop: " + i + "<br>";
+    i++;
+  } while (i <= 5);
+  document.getElementById("output").innerHTML = text;
+}
+
+function runForInLoop() {
+  const student = {name: "Muaz", age: 25, city: "Ctg"};
+  let text = "";
+  for (let key in student) {
+    text += key + ": " + student[key] + "<br>";
+  }
+  document.getElementById("output").innerHTML = text;
+}
+
+function runForOfLoop() {
+  const fruits = ["Apple", "Banana", "Orange"];
+  let text = "";
+  for (let fruit of fruits) {
+    text += "Fruit: " + fruit + "<br>";
+  }
+  document.getElementById("output").innerHTML = text;
+}
