7f7bbe7a6e976c37f405a05268b1eb7138780623
muaz64 <muazmuhammad21@gmail.com>
2025-06-25 23:36:26 +0600

    js cls15 1

---
+++ JavaScript/Class15/script.js
@@ -0,0 +1,74 @@
+// var a = 'muaz muhammad';
+// console.log('my name is:  ' + a);
+// a = 'Musab Muhammad';
+//  console.log('Now the name is:  ' + a);
+ 
+ 
+//  let b = 'muaz muhammad';
+// console.log('my name is:  ' + a);
+// b = 'Musab Muhammad';
+//  console.log('Now the name is:  ' + a);
+
+//  const c = 'muaz muhammad';
+// console.log('my name is:  ' + a);
+// c = 'Musab Muhammad';
+//  console.log('Now the name is:  ' + a);
+// function es5var(){
+//     if(true){
+//         var a = 5 ;
+
+//     }
+//     console.log('value: ' + a);
+// }
+// es5var();
+
+// function es6var(){
+//     if(true){
+//         let b = 5 ;
+
+//     }
+//     console.log('value: ' + a);
+// }
+// es6var();
+//  var i =13 ; for(var i=0 ; i < 5; i++){
+//     console.log(i)
+//  }
+//  console.log ('value: '+ i);
+
+
+// let i6=13 ; 
+// for(let i6=0; i6 < 5; i6++) {
+//     console.log(i6);
+// }
+// console.log ('value: '+ i6);
+// (function aDemoFunc() {
+//     var msg ='we are iife';
+//     console.log(msg);
+// })();
+// {
+//     let a =5;
+//     let b =10;
+//     const pi =3.1416;
+//     console.log(a + b + pi);
+// }
+//  console.log(a + b + pi);
+// var name = 'Muaz Muahammad'
+// age =25;
+// work = 'teacher';
+// console.log('My name is ' + name + 'and i\'m' + age + 'years old! Currently i\'m a '+ work);
+// const name6 = 'Muaz Muahammad'
+// age6 =25;
+// work6 = 'teacher';
+// console.log(`My name is ${name6} and i am ${age} year old! Currenty i am a ${work}`);
+const start = 'My name is kabir';
+start.startsWith('My');
+
+const end = 'My name is kabir';
+end.endsWith('r');
+
+const inc = "i love js but hate coding";
+inc.includes('js');
+
+const rpt = 'We are happy!';
+rpt.repeat(5);
+
