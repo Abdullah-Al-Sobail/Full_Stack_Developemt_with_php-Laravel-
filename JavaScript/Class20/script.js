d653a400075ccfb5292a3f7a171aa416c140d38b
muaz64 <muazmuhammad21@gmail.com>
2025-08-11 22:23:52 +0600

    js cls20 upd

---
+++ JavaScript/Class20/script.js
@@ -0,0 +1,21 @@
+// var box1 = document.querySelector('#box1');
+
+// document.getElementById('box2');
+
+// document.querySelector('.box')
+// document.getElementsByClassName('box')
+// document.querySelectorAll('.box')
+// document.querySelector('div')
+// document.getElementsByTagName('h1')
+// document.querySelector('li:first-child');
+
+// var parentElement = document.getElementById('dom')
+ var selectBox = document.getElementById('box1');
+
+ var newPara = document.createElement('p');
+  var newDiv = document.createElement('div');
+  var text1 = "Muaz Muhammad";
+  newPara.textContent = 'His name is: ' + text1 ;
+
+newDiv.innerHTML = "<p>Dom is fun</p>";
+newDiv.classList.add('new-div')
